5169e4a26f7465a181d36057e45319b412316281
surendranaik123 <131369740+surendranaik123@users.noreply.github.com>
2024-02-22 13:00:23 +0530

    chan

--- server/index.js
+++ server/index.js
@@ -20,13 +20,7 @@ app.use(express.json());
 // app.use(cors());
 
 // Apply middleware
-app.use(cors(
-  {
-    origin:["https://website-delta-two-63.vercel.app"],
-    methods:["Post","Get"],
-    credentials:true 
-
-}));
+app.use(cors());
 
 app.use(express.json({ limit: '10mb' }));
 
